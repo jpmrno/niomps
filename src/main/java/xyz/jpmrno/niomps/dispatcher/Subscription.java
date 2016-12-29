32ae7c9b2ca9bf500eaa049df2e0b007d82df94b
Juan <jpmrno@gmail.com>
2016-12-28 23:07:23 -0300

    Removed guava preconditions

--- src/main/java/xyz/jpmrno/niomps/dispatcher/Subscription.java
+++ src/main/java/xyz/jpmrno/niomps/dispatcher/Subscription.java
@@ -4,29 +4,30 @@ import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectableChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.util.Objects;
 
 public class Subscription {
     private final Selector selector;
     private final SelectableChannel channel;
-    private final Subscriber handler;
 
     private boolean cancelled = false;
 
     public Subscription(Selector selector, SelectableChannel channel, Subscriber handler)
             throws ClosedChannelException {
-        this.selector = checkNotNull(selector, "Selector can't be null");
-        this.channel = checkNotNull(channel, "Channel can't be null");
-        this.handler = checkNotNull(handler, "Subscriber can't be null");
+
+        this.selector = Objects.requireNonNull(selector, "Selector can't be null");
+        this.channel = Objects.requireNonNull(channel, "Channel can't be null");
+        Objects.requireNonNull(handler, "Subscriber can't be null");
 
         channel.register(selector, DispatcherOperation.NONE.getValue(), handler);
     }
 
-    void add(final DispatcherOperation op) throws SubscriptionException {
+    void add(final DispatcherOperation op) {
         if (cancelled) {
             return;
         }
 
-        checkNotNull(op, "Operation can't be null");
+        Objects.requireNonNull(op, "Operation can't be null");
 
         SelectionKey key = channel.keyFor(selector);
 
@@ -42,7 +43,7 @@ public class Subscription {
             return;
         }
 
-        checkNotNull(op, "Operation can't be null");
+        Objects.requireNonNull(op, "Operation can't be null");
 
         SelectionKey key = channel.keyFor(selector);
 
